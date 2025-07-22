1919b717afa493302133213c30717af20689406a
Tiago Coelho <tiagocoelho33@gmail.com>
2025-07-22 18:52:17 -0300

    init xcode project

---
+++ MiniMeli/MiniMeli/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MiniMeli
+//
+//  Created by Tiago P. Coelho on 22/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MiniMeli/MiniMeliTests/MiniMeliTests.swift
@@ -0,0 +1,17 @@
+//
+//  MiniMeliTests.swift
+//  MiniMeliTests
+//
+//  Created by Tiago P. Coelho on 22/07/25.
+//
+
+import Testing
+@testable import MiniMeli
+
+struct MiniMeliTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
