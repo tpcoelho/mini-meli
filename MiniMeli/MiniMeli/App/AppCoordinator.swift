ca14f06150721742e940fb86183b5abb875f2158
Tiago Coelho <tiagocoelho33@gmail.com>
2025-07-24 20:51:06 -0300

    initial folders

---
+++ MiniMeli/MiniMeli/App/AppCoordinator.swift
@@ -0,0 +1,30 @@
+//
+//  AppCoordinator.swift
+//  MiniMeli
+//
+//  Created by Tiago P. Coelho on 22/07/25.
+//
+
+
+import UIKit
+
+protocol BaseCoordinator: AnyObject {
+    var navigationController: UINavigationController { get set }
+    
+    func start()
+}
+
+/// App coordinator is the only one coordinator which will exist during app's life cycle
+class AppCoordinator: BaseCoordinator {
+    var navigationController: UINavigationController
+
+    init(_ navigationController: UINavigationController) {
+        self.navigationController = navigationController
+        navigationController.setNavigationBarHidden(true, animated: true)
+    }
+    
+    func start() {
+        let vc = ViewController()
+        navigationController.pushViewController(vc, animated: false)
+    }
+}
